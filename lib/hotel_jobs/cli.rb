12427a230b0761d47a2762c01b57575f8bfec9d6
Alison <reillyamr@gmail.com>
2019-01-18 11:47:04 -0500

    Update cli.rb
    test initial setup with puts statement

---
+++ lib/hotel_jobs/cli.rb
@@ -0,0 +1,12 @@
+#file uner hotel_jobs as cli.rb
+#is a means of namespacing so cli under any other user is specific
+#to this gem and not some other user's gem and environment setup
+
+
+class HotelJobs::CLI
+
+  #code to see if all is setup correctly
+  def start
+    puts "Hello"
+  end
+end 
